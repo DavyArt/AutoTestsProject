f3427771560229969e398b91d85646a38a9387a8
Artsiom Hrymus <artyomgrimus@gmail.com>
2024-09-27 17:08:22 +0300

    vbnv

--- APITests/Models/DayForecastModel.cs
+++ APITests/Models/DayForecastModel.cs
@@ -8,6 +8,5 @@ namespace UITests.Models
     {
         public long Id { get; set; }
         public string Weather_state_name { get; set; }
-        public string Applicable_date { get; set; }
     }
 }
